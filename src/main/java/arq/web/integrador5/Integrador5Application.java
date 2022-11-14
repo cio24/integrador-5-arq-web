f1923c2ef19f1d2789b9149d64044675fffef20a
Casado Horacio <hcasado@santandertecnologia.com.ar>
2022-11-14 11:16:45 -0300

    first commit

---
+++ src/main/java/arq/web/integrador5/Integrador5Application.java
@@ -0,0 +1,13 @@
+package arq.web.integrador5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Integrador5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Integrador5Application.class, args);
+	}
+
+}
---
+++ src/test/java/arq/web/integrador5/Integrador5ApplicationTests.java
@@ -0,0 +1,13 @@
+package arq.web.integrador5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integrador5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
