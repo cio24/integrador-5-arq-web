e64dd850b92648226978df44b7ce74012dd28518
Casado Horacio <hcasado@santandertecnologia.com.ar>
2022-11-14 16:18:29 -0300

    add swagger dor careers

--- src/main/java/arq/web/integrador5/controlllers/CareerController.java
+++ src/main/java/arq/web/integrador5/controlllers/CareerController.java
@@ -2,13 +2,15 @@ package arq.web.integrador5.controlllers;
 
 import arq.web.integrador5.DTO.CareerDTO;
 import arq.web.integrador5.services.CareerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-
+@Api(tags = "Carreras")
 @RestController
 @RequestMapping("/careers")
 public class CareerController {
@@ -16,11 +18,13 @@ public class CareerController {
     @Autowired
     CareerService careerSerivce;
 
+    @ApiOperation(value = "crea una nueva carrera")
     @PostMapping
     public CareerDTO save(@RequestBody CareerDTO c){
         return careerSerivce.save(c);
     }
 
+    @ApiOperation(value = "obtiene un arreglo de carreras")
     @GetMapping()
     public List<CareerDTO> getMany(@RequestParam String withStudents, @RequestParam String sortBy){
         if(withStudents.equals("true") && sortBy.equals("enrolledAmount"))
