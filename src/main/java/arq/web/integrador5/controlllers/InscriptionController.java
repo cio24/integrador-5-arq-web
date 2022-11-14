dc1ea2331480e9e64f2c517f5e59f03ba2f2f168
Casado Horacio <hcasado@santandertecnologia.com.ar>
2022-11-14 19:01:59 -0300

    add swagger documentation for rest of controllers

--- src/main/java/arq/web/integrador5/controlllers/InscriptionController.java
+++ src/main/java/arq/web/integrador5/controlllers/InscriptionController.java
@@ -3,10 +3,13 @@ package arq.web.integrador5.controlllers;
 import arq.web.integrador5.DTO.CareerReportDTO;
 import arq.web.integrador5.DTO.InscriptionDTO;
 import arq.web.integrador5.services.InscriptionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Api(tags = "Inscripciones")
 @RestController
 @RequestMapping("/inscriptions")
 public class InscriptionController {
@@ -14,12 +17,13 @@ public class InscriptionController {
     @Autowired
     private InscriptionService inscriptionService;
 
+    @ApiOperation(value = "crea una nueva inscripción de un estudiante en una carrera")
     @PostMapping()
     public InscriptionDTO save(@RequestBody InscriptionDTO iDTO){
         return inscriptionService.save(iDTO);
     }
 
-
+    @ApiOperation(value = "obtiene un arreglo de reportes con información por año de cada carrera")
     @GetMapping("/reports")
     public List<CareerReportDTO> getReports(){
             return inscriptionService.getReports();
--- src/main/java/arq/web/integrador5/controlllers/StudentController.java
+++ src/main/java/arq/web/integrador5/controlllers/StudentController.java
@@ -2,6 +2,8 @@ package arq.web.integrador5.controlllers;
 
 import arq.web.integrador5.DTO.StudentDTO;
 import arq.web.integrador5.services.StudentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@Api(tags = "Estudiantes")
 @RestController
 @RequestMapping("/students")
 public class StudentController {
@@ -16,16 +19,19 @@ public class StudentController {
     @Autowired
     StudentService studentSerivce;
 
+    @ApiOperation(value = "crea un nuevo estudiante")
     @PostMapping()
     public StudentDTO save(@RequestBody StudentDTO s){
         return studentSerivce.save(s);
     }
 
+    @ApiOperation(value = "obtiene un estudiando filtrado por número de libreta")
     @GetMapping("/{id}")
     public StudentDTO getByBookName(@PathVariable String id){
         return studentSerivce.getByBookNumber(Integer.valueOf(id));
     }
 
+    @ApiOperation(value = "obtiene un arreglo de estudiantes")
     @GetMapping()
     public List<StudentDTO> getMany(@RequestParam(required = false) String gender, @RequestParam(required = false) String sortBy,
                                     @RequestParam(required = false) Integer careerId, @RequestParam(required = false) String city){
